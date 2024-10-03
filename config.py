eaef0836ead0f0d0959c52112c9bfb35bfb17df2
aaru2075 <aryarai1569@protonmail.com>
2024-10-04 00:43:52 +0530

    config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ env_vars = {
   #Get For @BotFather
   "BOT_TOKEN": "8142342939:AAH3QFfhslgV2tYWceLUYSyBw6kj_f9_yaY",
   # Get For tembo.io
-  "DATABASE_URL_PRIMARY": "",
+  "DATABASE_URL_PRIMARY": "postgresql://postgres:00IXtNloxpgkCFrA@distractedly-stellar-trembler.data-1.use1.tembo.io:5432/postgres",
   # Logs Channel Username Without @
   "CACHE_CHANNEL": "",
   # Force Subs Channel username without @
@@ -14,7 +14,7 @@ env_vars = {
   # {chap_num}: Chapter Number
   # {chap_name} : Manga Name
   # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
-  "FNAME": ""
+  "FNAME": "[MC] [{chap_num}] [{chap_name}] @Manga_Campus"
 }
 
 dbname = env_vars.get('DATABASE_URL_PRIMARY') or env_vars.get('DATABASE_URL') or 'sqlite:///test.db'
